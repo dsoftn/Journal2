f9237923e99803594139d762f793df6113e7c5d4
DsoftN <dsoftn@gmail.com>
2024-12-14 01:02:39 +0100

    Refactored CSS files

--- journal2/src/main/java/com/dsoftn/controllers/MsgBoxController.java
+++ journal2/src/main/java/com/dsoftn/controllers/MsgBoxController.java
@@ -1,5 +1,9 @@
 package com.dsoftn.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -43,6 +47,33 @@ public class MsgBoxController implements IBaseController {
     // Variables
     private Stage stage;
     private List<MsgBoxButton> buttons = new ArrayList<>() { { add(MsgBoxButton.OK); } };
+    private boolean showHeader = false;
+    private MsgBoxIcon headerIcon = MsgBoxIcon.APP;
+    private String headerText = "";
+    private boolean showContent = true;
+    private MsgBoxIcon contentIcon = MsgBoxIcon.APP;
+    private String contentText = "";
+
+
+    // FXML
+    @FXML
+    private Label lblIconHeader; // Icon in header
+    @FXML
+    private Label lblIconContent; // Icon in content
+    @FXML
+    private VBox vBoxHeader; // Header section
+    @FXML
+    private VBox vBoxContent; // Content section
+    @FXML
+    private Button btnFind; // Find button
+    @FXML
+    private Button btnYes; // Yes button
+    @FXML
+    private Button btnNo; // No button
+    @FXML
+    private Button btnOk; // Ok button
+    @FXML
+    private Button btnCancel; // Cancel button
 
 
     // Public methods
